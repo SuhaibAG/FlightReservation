4389b275924e41ccd55e7b8f7a1bb59348600366
Waleed ALsafari <waleedalsafari@gmail.com>
2024-05-04 00:56:31 +0300

    create Ticket, Model, Controller and View classes

---
+++ src/Controller.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class Controller {
+    Model model;
+
+    Controller(Model model){
+        this.model = model;
+    }
+
+    public void bookTicket(){
+    }
+
+    public void scheduleFlight(){
+       
+    }
+
+    public void viewTicketList(){
+
+    }
+}
---
+++ src/Model.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class Model {
+    ArrayList<FlightSchedular> schedule;
+    ArrayList<Ticket> ticketList;
+}
