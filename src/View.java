feb02bb2da5250b860ce9727f9f8389eaa1f0225
Suhaib Alghamdi <suhaib.ag.49@gmail.com>
2024-05-06 15:51:13 +0300

    added things to view

--- src/View.java
+++ src/View.java
@@ -1,3 +1,11 @@
 public class View {
-    
+    public void printChoices(){
+        System.out.println("Welcome to the Flight Reservation system.");
+        System.out.println("Enter (1) to make a new flight, Enter (2) to book a ticket to an existing flight");
+    }
+
+    public void flightpPaths(){
+        System.out.println("Enter the Flight path: ");
+        System.out.println("JedDam, JedRiy, DamJed, DamRiy, RiyJed, RiyDam");
+    }
 }
